f6b107a6cf89a22701d7b89e69e6000753ca6340
Mert Bayır <118671917+mertbayirs4u@users.noreply.github.com>
2023-12-10 20:06:33 +0300

    obje_tespit.py scriptine güncelleme yapıldı.

--- scripts/obje_tespit.py
+++ scripts/obje_tespit.py
@@ -1,38 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import cv2
-import numpy as np
-import rospy
-from find_object_2d.msg import ObjectsStamped
-from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist
-from cv_bridge import CvBridge
 
-class objeTespit():
+import rospy
+import cv2
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import LaserScan
+from find_object_2d.msg import ObjectsStamped
+
+class Kamera():
     def __init__(self):
-        rospy.init_node("obje_tespit")
+        rospy.init_node("kamera_dugumu")
         self.bridge = CvBridge()
-        rospy.Subscriber("camera/rgb/image_raw", Image, self.nesne_tespit)
-    
+        rospy.Subscriber("camera/rgb/image_raw",Image,self.kameraCallback)
+        rospy.Subscriber("objectsStamped",ObjectsStamped,self.kameraCallback)
         rospy.spin()
         
+    def kameraCallback(self,mesaj):
+        img = self.bridge.imgmsg_to_cv2(mesaj,"mono8")
+        img2 = self.bridge.imgmsg_to_cv2(mesaj,"bgr8")
+        self.nesne_id = mesaj.objects.data[0]
+        a,esiklenmis = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+        sinirlar,hiyerarsi = cv2.findContours(esiklenmis,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+        cv2.drawContours(img2,sinirlar,-1,(255,0,0),4)
+        cnt = sinirlar[0]
+        M = cv2.moments(cnt)
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+        print("Nesne X ekseni = ",cx," /  Nesne Y ekseni = ",cy,"  / Nesne Id = ", self.nesne_id  )
 
-    def nesne_tespit(self,mesaj):
-            
-            img = self.bridge.imgmsg_to_cv2(mesaj,"mono8")
-            img2 = self.bridge.imgmsg_to_cv2(mesaj,"bgr8")
-            ret,esikli = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)                                                              
-            sinirlar,hiyerarsi = cv2.findContours(esikli,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(img2,sinirlar,-1,(255,0,0),5)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-            cnt = sinirlar[0]
-            M=cv2.moments(cnt)
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            print("Cismin Agirlik Merkezi = ",cx,cy,"  Nesne ID = ")
-     
+Kamera()
 
-objeTespit()
+
 
 
         
